580c50e0e9a1f889c91812a8c6dad50279870495
Jimmy Royer <jimleroyer@gmail.com>
2023-12-01 23:03:18 -0500

    Initial Haskell setup using a vscode devcontainer

---
+++ app/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import qualified Day1 (part1)
+
+main :: IO ()
+main = Day1.part1
---
+++ src/Day1.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Day1 (part1, part2) where
+
+part1 :: IO ()
+part1 = putStrLn "Hello, Part1!"
+
+part2 :: IO ()
+part2 = putStrLn "Hello, Part2!"
